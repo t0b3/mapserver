f14f8f3de960aebb15bc9de2f3dfd7e6205337fd
sethg <sethg@geographika.co.uk>
2020-05-24 12:21:13 +0200

    Remove all refs to classObj in api

--- mapserver-api.h
+++ mapserver-api.h
@@ -24,7 +24,7 @@ typedef struct imageObj imageObj;
 
 mapObj* umnms_new_map(char *filename);
 layerObj* umnms_new_layer(mapObj *map);
-classObj* umnms_new_class(layerObj *layer);
+//classObj* umnms_new_class(layerObj *layer);
 styleObj* umnms_new_style(classObj *theclass);
 labelObj* umnms_new_label(classObj *theclass);
 
