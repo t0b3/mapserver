2dc4c46c89defcd043d907eedcecb2b8163eca54
sethg <sethg@geographika.co.uk>
2021-06-07 00:47:07 +0200

    Use xmlFree

--- mapows.c
+++ mapows.c
@@ -175,8 +175,18 @@ static int msOWSPreParseRequest(cgiRequestObj *request,
     }
 
     /* Get service, version and request from root */
-    ows_request->service = msStrdup((char *)xmlGetProp(root, BAD_CAST "service"));
-    ows_request->version = msStrdup((char *)xmlGetProp(root, BAD_CAST "version"));
+    xmlChar* serviceTmp = NULL;
+    if (serviceTmp != NULL) {
+        ows_request->service = (char *)serviceTmp;
+        xmlFree(serviceTmp);
+    }
+
+    xmlChar* versionTmp = NULL;
+    if (versionTmp != NULL) {
+        ows_request->version = (char *)versionTmp;
+        xmlFree(versionTmp);
+    }
+
     ows_request->request = msStrdup((char *) root->name);
 
 #else
