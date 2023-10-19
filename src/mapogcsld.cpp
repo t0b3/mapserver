f0f58a009b52979ba2084b2ffd03b53dc6dacb99
sethg <sethg@geographika.co.uk>
2023-10-19 14:24:01 +0200

    Handle <>

--- src/mapogcsld.cpp
+++ src/mapogcsld.cpp
@@ -4570,7 +4570,8 @@ static char *msSLDGetComparisonValue(const char *pszExpression) {
     pszValue = msStrdup("PropertyIsLike");
   else if (strstr(pszExpression, ">=") || strstr(pszExpression, " ge "))
     pszValue = msStrdup("PropertyIsGreaterThanOrEqualTo");
-  else if (strstr(pszExpression, "!=") || strstr(pszExpression, " ne "))
+  else if (strstr(pszExpression, "!=") || strstr(pszExpression, " ne ") ||
+           strstr(pszExpression, "<>"))
     pszValue = msStrdup("PropertyIsNotEqualTo");
   else if (strstr(pszExpression, "=") || strstr(pszExpression, " eq "))
     pszValue = msStrdup("PropertyIsEqualTo");
@@ -4748,6 +4749,7 @@ static char *msSLDGetAttributeNameOrValue(const char *pszExpression,
   char cCompare = '=';
   char szCompare[3] = {0};
   char szCompare2[3] = {0};
+  char szCompare3[3] = {0};
   int bOneCharCompare = -1, nTokens = 0, nLength = 0;
   int iValue = 0, i = 0, iValueIndex = 0;
   int bStartCopy = 0, iAtt = 0;
@@ -4759,6 +4761,7 @@ static char *msSLDGetAttributeNameOrValue(const char *pszExpression,
 
   szCompare[0] = '\0';
   szCompare2[0] = '\0';
+  szCompare3[0] = '\0';
 
   if (strcasecmp(pszComparionValue, "PropertyIsEqualTo") == 0) {
     cCompare = '=';
@@ -4776,6 +4779,10 @@ static char *msSLDGetAttributeNameOrValue(const char *pszExpression,
     szCompare2[0] = '!';
     szCompare2[1] = '=';
     szCompare2[2] = '\0';
+
+    szCompare3[0] = '<';
+    szCompare3[1] = '>';
+    szCompare3[2] = '\0';
 
     bOneCharCompare = 0;
   } else if (strcasecmp(pszComparionValue, "PropertyIsLike") == 0) {
@@ -4859,15 +4866,17 @@ static char *msSLDGetAttributeNameOrValue(const char *pszExpression,
       if ((pszExpression[i] != szCompare[0] ||
            pszExpression[i] != toupper(szCompare[0])) &&
           (pszExpression[i] != szCompare2[0] ||
-           pszExpression[i] != toupper(szCompare2[0])))
-
-      {
+           pszExpression[i] != toupper(szCompare2[0])) &&
+          (pszExpression[i] != szCompare3[0] ||
+           pszExpression[i] != toupper(szCompare3[0]))) {
         pszAttributeName[iValue++] = pszExpression[i];
       } else {
         if (((pszExpression[i + 1] == szCompare[1] ||
               pszExpression[i + 1] == toupper(szCompare[1])) ||
              (pszExpression[i + 1] == szCompare2[1] ||
-              pszExpression[i + 1] == toupper(szCompare2[1]))) &&
+              pszExpression[i + 1] == toupper(szCompare2[1])) ||
+             (pszExpression[i + 1] == szCompare3[1] ||
+              pszExpression[i + 1] == toupper(szCompare3[1]))) &&
             (pszExpression[i + 2] == ' ')) {
           iValueIndex = i + 3;
           pszAttributeValue = msStrdup(pszExpression + iValueIndex);
@@ -4950,11 +4959,12 @@ static char *msSLDGetAttributeNameOrValue(const char *pszExpression,
       pszFinalAttributeValue[iAtt] = '\0';
     }
 
+    msStringTrimBlanks(pszFinalAttributeValue);
+
     /*trim  for regular expressions*/
     if (strlen(pszFinalAttributeValue) > 2 &&
         strcasecmp(pszComparionValue, "PropertyIsLike") == 0) {
       int len = strlen(pszFinalAttributeValue);
-      msStringTrimBlanks(pszFinalAttributeValue);
       if (pszFinalAttributeValue[0] == '/' &&
           (pszFinalAttributeValue[len - 1] == '/' ||
            (pszFinalAttributeValue[len - 1] == 'i' &&
